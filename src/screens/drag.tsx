b3edc08d8d0baa60422958969aeb7bf30c2a0e78
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-08-28 15:34:46 +0200

    Drag

--- src/screens/drag.tsx
+++ src/screens/drag.tsx
@@ -1,20 +1,59 @@
-import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-gesture-handler";
+import React, { useRef } from 'react';
+import { StyleSheet, View, Text, Animated, PanResponder } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Drag() {
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        pan.extractOffset();
+      },
+    }),
+  ).current;
+
+
   return (
-    <View style={styles.container}>
-        <View>
-            <Text>Drag</Text>
-        </View>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Drag this box!</Text>
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        onStartShouldSetResponder={panResponder.panHandlers.onStartShouldSetResponder}
+        onMoveShouldSetResponder={panResponder.panHandlers.onMoveShouldSetResponder}
+        onMoveShouldSetResponderCapture={panResponder.panHandlers.onMoveShouldSetResponderCapture}
+        onStartShouldSetResponderCapture={panResponder.panHandlers.onStartShouldSetResponderCapture}
+        onResponderGrant={panResponder.panHandlers.onResponderGrant}
+        onResponderMove={panResponder.panHandlers.onResponderMove}
+        onResponderRelease={panResponder.panHandlers.onResponderRelease}
+        onResponderTerminate={panResponder.panHandlers.onResponderTerminate}
+        onResponderTerminationRequest={panResponder.panHandlers.onResponderTerminationRequest}
+      >
+        <View style={styles.box} />
+      </Animated.View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: 'bold',
+  },
+  box: {
+    height: 150,
+    width: 150,
+    backgroundColor: 'grey',
+    borderRadius: 5,
+  },
 })
