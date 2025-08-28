9f49aedeeaf072d76d6168fe560e2fcc9c931831
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-08-28 13:42:26 +0200

    Tap gesture

--- src/screens/tap.tsx
+++ src/screens/tap.tsx
@@ -1,20 +1,62 @@
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-gesture-handler";
+import { Gesture, GestureDetector, Text } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 export default function Tap() {
+  const scale = useSharedValue(0);
+
+  const tapGesture = Gesture.Tap()
+    .numberOfTaps(2)
+    .onEnd((_, success) => {
+      "worklet";
+
+      if (success) {
+        scale.value = withSpring(scale.value === 0 ? 1 : 0, {
+          damping: 10,
+          stiffness: 200,
+        });
+      }
+    });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
   return (
     <View style={styles.container}>
-        <View>
-            <Text>Tap</Text>
-        </View>
+      <Text>Double Tap Box</Text>
+      <GestureDetector gesture={tapGesture}>
+        <Animated.View style={styles.body}>
+          <Animated.Image
+            source={require('../images/heart.png')}
+            style={[styles.image, animatedStyle]}
+          />
+        </Animated.View>
+      </GestureDetector>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  body: {
+    backgroundColor: '#5a5757ff',
+    borderRadius: 10,
+    marginTop: 20,
+    height: 200,
+    width: 200,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    top: 50,
+    left: 50,
+  },
 })
